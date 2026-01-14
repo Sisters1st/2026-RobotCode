8b00fa62919d3eda0e82134338347763dc9bb3e8
Owen Busler <owen1050@gmail.com>
2026-01-14 11:23:44 -0500

    moved heading control to propper place

--- 2026-ROBOTCODE-8513/src/main/java/frc/robot/Subsystems/Drivebase.java
+++ 2026-ROBOTCODE-8513/src/main/java/frc/robot/Subsystems/Drivebase.java
@@ -3,9 +3,12 @@ package frc.robot.Subsystems;
 import java.io.File;
 import java.io.IOException;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Filesystem;
+import frc.robot.Robot;
 import frc.robot.Logic.Settings;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
@@ -14,15 +17,31 @@ public class Drivebase {
 
     public SwerveDrive yagslDrive;
 
+    public Rotation2d goalHeading = new Rotation2d();
+
+    public PIDController rotationPidController = new PIDController(Settings.DrivebaseSettings.RotationPIDConstants.kP,
+            Settings.DrivebaseSettings.RotationPIDConstants.kI,
+            Settings.DrivebaseSettings.RotationPIDConstants.kD);
+
     public Drivebase() {
         File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
         try {
             yagslDrive = new SwerveParser(swerveJsonDirectory)
-                            .createSwerveDrive(Settings.DrivebaseSettings.maxVelocityMPS, 
-                                                new Pose2d(8.25, 4, new Rotation2d(0)));
+                    .createSwerveDrive(Settings.DrivebaseSettings.maxVelocityMPS,
+                            new Pose2d(8.25, 4, new Rotation2d(0)));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void drive(Translation2d translation2d, Rotation2d heading, boolean fR) {
+        double angleError = Robot.drivebase.yagslDrive.getOdometryHeading().minus(heading).getDegrees();
+        double rotationCorrection = rotationPidController.calculate(angleError, 0);
+
+        Robot.drivebase.yagslDrive.drive(translation2d,
+                rotationCorrection,
+                fR,
+                false);
     }
 
 }
--- 2026-RobotCode-8513/src/main/java/frc/robot/Logic/TeleopController.java
+++ 2026-RobotCode-8513/src/main/java/frc/robot/Logic/TeleopController.java
@@ -1,8 +1,5 @@
 package frc.robot.Logic;
 
-import java.util.Set;
-
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.Joystick;
@@ -13,16 +10,16 @@ public class TeleopController {
     Joystick driverJoystick = new Joystick(Settings.TeleopSettings.DriverJoystick.port);
     public boolean fieldRelative = true;
 
-    public Rotation2d goalHeading = new Rotation2d();
-    public PIDController rotationPidController = new PIDController(Settings.DrivebaseSettings.RotationPIDConstants.kP,
-            Settings.DrivebaseSettings.RotationPIDConstants.kI,
-            Settings.DrivebaseSettings.RotationPIDConstants.kD);
-
     public TeleopController() {
 
     }
 
     public void teleopLoop() {
+        driveWithJoystick();
+    }
+
+    public void driveWithJoystick(){
+
         double xJoystickValule = driverJoystick.getRawAxis(Settings.TeleopSettings.DriverJoystick.xAxis);
         double yJoystickValule = driverJoystick.getRawAxis(Settings.TeleopSettings.DriverJoystick.yAxis);
         double rJoystickValule = -driverJoystick.getRawAxis(Settings.TeleopSettings.DriverJoystick.rAxis);
@@ -44,21 +41,15 @@ public class TeleopController {
 
         Translation2d robotVelocity = new Translation2d(xJoystickValule * Settings.DrivebaseSettings.maxVelocityMPS,
                 yJoystickValule * Settings.DrivebaseSettings.maxVelocityMPS);
+        Rotation2d goalHeading = Robot.drivebase.yagslDrive.getOdometryHeading();
 
         if (!(Math.abs(rJoystickValule) < Settings.TeleopSettings.DriverJoystick.deadband)) {
-            goalHeading = Robot.drivebase.yagslDrive.getOdometryHeading()
-                    .plus(new Rotation2d(
+            goalHeading = goalHeading.plus(new Rotation2d(
                             rJoystickValule * Settings.TeleopSettings.DriverJoystick.rotationalJoystickSensitivity));
-
         }
 
-        double angleError = Robot.drivebase.yagslDrive.getOdometryHeading().minus(goalHeading).getDegrees();
-        double rotationCorrection = rotationPidController.calculate(angleError, 0);
+        Robot.drivebase.drive(robotVelocity, goalHeading, fieldRelative);
 
-        Robot.drivebase.yagslDrive.drive(robotVelocity,
-                rotationCorrection,
-                fieldRelative,
-                false);
     }
 
 }
