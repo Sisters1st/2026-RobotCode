00e7514ceaad44569faacabcb5bb518d0237f414
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2026-08-17 17:51:21 -0400

    end of 8/17 - code crashing

--- 2026-ROBOTCODE-8513/src/main/java/frc/robot/Robot.java
+++ 2026-ROBOTCODE-8513/src/main/java/frc/robot/Robot.java
@@ -86,7 +86,7 @@ public class Robot extends LoggedRobot {
     }
 
     Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may
-                    // be added.
+    //                 // be added.
 
     SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
 
@@ -125,7 +125,7 @@ public class Robot extends LoggedRobot {
       Robot.intake.intakeDeployController.reset(Robot.intake.intakeDeployMotor.getPosition().getValueAsDouble());
     }
     
-    Logger.recordOutput("time", Timer.getFPGATimestamp());
+    // Logger.recordOutput("time", Timer.getFPGATimestamp());
 
   }
 
